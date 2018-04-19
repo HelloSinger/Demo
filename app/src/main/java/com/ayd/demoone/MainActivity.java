512b1f0143335e42538f6c7c2b186f41c1258474
anyaodong <1012592040@qq.com>
2018-04-19 19:00:21 +0800

    233333

--- app/src/main/java/com/ayd/demoone/MainActivity.java
+++ app/src/main/java/com/ayd/demoone/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private String name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
